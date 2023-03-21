daccc59cf9507800a2ce87e6db281ca968156440
SumiAkter2 <akteralisha36@gmail.com>
2023-03-21 15:55:16 +0600

    banner done

---
+++ src/components/Discover/DisCover.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from './Banner';
+
+const Discover = () => {
+    return (
+        <div>
+            <Banner/>
+        </div>
+    );
+};
+
+export default Discover;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Banner from '../components/Discover/Banner'
-
+import DisCover from '../components/Discover/DisCover'
 const Discover = () => {
   return (
     <div>
-      <h2>Discover</h2>
-     <Banner/>
+      <h2 className='text-3xl my-8 font-bold'>Discover</h2>
+     <DisCover/>
     </div>
   )
 }
